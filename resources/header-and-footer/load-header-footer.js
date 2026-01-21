faa81da1af93ac7f31a991152b076b38890922e5
Noor Mohammad Siam <121155007+noormohammadsiam@users.noreply.github.com>
2026-01-21 17:24:51 +0600

    Update load-header-footer.js

--- resources/header-and-footer/load-header-footer.js
+++ resources/header-and-footer/load-header-footer.js
@@ -2,7 +2,7 @@
         // Load header and footer
         document.addEventListener('DOMContentLoaded', function() {
             // Load header
-            fetch('https://mozammalhoque-group.pages.dev/resources/header-and-footer/header.html')
+            fetch('/resources/header-and-footer/header.html')
                 .then(response => response.text())
                 .then(data => {
                     document.getElementById('header-container').innerHTML = data;
@@ -10,7 +10,7 @@
                 });
             
             // Load footer
-            fetch('https://mozammalhoque-group.pages.dev/resources/header-and-footer/footer.html')
+            fetch('/resources/header-and-footer/footer.html')
                 .then(response => response.text())
                 .then(data => {
                     document.getElementById('footer-container').innerHTML = data;
